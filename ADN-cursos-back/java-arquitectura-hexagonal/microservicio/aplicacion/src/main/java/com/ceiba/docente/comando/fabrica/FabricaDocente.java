cb7cfada29fe4262310eb4c1b14ea34cadf0aa96
Sehuanez <sehuanez@gmail.com>
2022-05-08 21:16:20 -0500

    No crea el bean del RepositorioDocenteMysql V1.1

---
+++ ADN-cursos-back/java-arquitectura-hexagonal/microservicio/aplicacion/src/main/java/com/ceiba/docente/comando/fabrica/FabricaDocente.java
@@ -0,0 +1,19 @@
+package com.ceiba.docente.comando.fabrica;
+
+import com.ceiba.docente.comando.ComandoDocente;
+import com.ceiba.docente.modelo.entidad.Docente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FabricaDocente {
+
+    public Docente crear(ComandoDocente comandoDocente){
+        return new Docente(comandoDocente.getId(),
+                comandoDocente.getIdentificacion(),
+                comandoDocente.getNombre(),
+                comandoDocente.getApellido(),
+                comandoDocente.getDireccion(),
+                comandoDocente.getTelefono(),
+                comandoDocente.getCorreoElectronico());
+    }
+}
