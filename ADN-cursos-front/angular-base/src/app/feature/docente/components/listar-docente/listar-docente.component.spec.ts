fa98f2c943b11ea2b237a71c8af989646bce04fa
Sehuanez <sehuanez@gmail.com>
2022-05-31 23:37:06 -0500

    Listar docentes test

--- ADN-cursos-front/angular-base/src/app/feature/docente/components/listar-docente/listar-docente.component.spec.ts
+++ ADN-cursos-front/angular-base/src/app/feature/docente/components/listar-docente/listar-docente.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { RouterTestingModule } from '@angular/router/testing';
+
+import { Docente } from '@docente/shared/model/docente';
+import { DocenteService } from '@docente/shared/service/docente.service';
+import { HttpService } from '@core/services/http.service';
+import { of } from 'rxjs/internal/observable/of';
 
 import { ListarDocenteComponent } from './listar-docente.component';
 
 describe('ListarDocenteComponent', () => {
   let component: ListarDocenteComponent;
   let fixture: ComponentFixture<ListarDocenteComponent>;
+  let docenteService: DocenteService;
+  const listDocente: Docente[] = [
+    new Docente(1, '1234', 'test 1', 'test 1', 'test 1', 'test 1', 'test 1', 2),
+    new Docente(2, '12345', 'test 1', 'test 1', 'test 1', 'test 1', 'test 1', 2),
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListarDocenteComponent ]
+      declarations: [ ListarDocenteComponent ],
+      imports: [
+        HttpClientModule,
+        //RouterTestingModule.withRoutes(routes),
+        ReactiveFormsModule,
+        FormsModule,
+      ],
+      providers: [DocenteService, HttpService]
     })
     .compileComponents();
   });
@@ -16,6 +36,10 @@ describe('ListarDocenteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarDocenteComponent);
     component = fixture.componentInstance;
+    docenteService = TestBed.inject(DocenteService);
+    spyOn(docenteService, 'consultar').and.returnValue(
+      of(listDocente)
+    );
     fixture.detectChanges();
   });
 
