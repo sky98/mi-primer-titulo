1b539e24bfdff3995cb4246b43dd70ae43f0e4bb
rober.sehuanez.ceiba <rober.sehuanez@ceiba.com.co>
2022-06-03 16:53:17 -0500

    Agregando archivos po para los test funcionales en angular

--- ADN-cursos-front/angular-base/e2e/src/page/navbar/navbar.po.ts
+++ ADN-cursos-front/angular-base/e2e/src/page/navbar/navbar.po.ts
@@ -1,10 +1,31 @@
 import { by, element } from 'protractor';
 
 export class NavbarPage {
-    linkHome = element(by.xpath('/html/body/app-root/app-navbar/nav/a[1]'));
-    linkProducto = element(by.xpath('/html/body/app-root/app-navbar/nav/a[2]'));
+    //linkHome = element(by.xpath('/html/body/app-root/app-navbar/nav/a[1]'));
+    //linkProducto = element(by.xpath('/html/body/app-root/app-navbar/nav/a[2]'));
+    linkAlumno = element(by.xpath('/html/body/app-root/app-navbar/nav/a[1]'));
+    linkDocente = element(by.xpath('/html/body/app-root/app-navbar/nav/a[2]'));
+    linkCurso = element(by.xpath('/html/body/app-root/app-navbar/nav/a[3]'));
+    linkClase = element(by.xpath('/html/body/app-root/app-navbar/nav/a[4]'));
+    linkHorario = element(by.xpath('/html/body/app-root/app-navbar/nav/a[5]'));
+    linkSalon = element(by.xpath('/html/body/app-root/app-navbar/nav/a[6]'));
 
-    async clickBotonProductos() {
-        await this.linkProducto.click();
+    async clickBotonAlumnos() {
+        await this.linkAlumno.click();
+    }
+    async clickBotonDocentes() {
+        await this.linkDocente.click();
+    }
+    async clickBotonCursos() {
+        await this.linkCurso.click();
+    }
+    async clickBotonClases() {
+        await this.linkClase.click();
+    }
+    async clickBotonHorarios() {
+        await this.linkHorario.click();
+    }
+    async clickBotonSalones() {
+        await this.linkSalon.click();
     }
 }
